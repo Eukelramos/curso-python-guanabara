8f47e0308d981207c480b792552179950330a387
Eukelramos <163541107+Eukelramos@users.noreply.github.com>
2026-07-17 10:25:53 +0100

    Desafio 33 - Maior e menor número. Comparação usando múltiplos ifs independentes (não encadeados)

---
+++ Estudos/Condiçoes (pt 1)/desafio 33.py
@@ -0,0 +1,18 @@
+# Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+
+maior = n1
+menor = n1
+
+if n2 > maior:
+    maior = n2
+if n3 > maior:
+    maior = n3
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3
+print(f'O maior numero é {maior} e o menor {menor}')
